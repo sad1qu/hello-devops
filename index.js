3433240bedbf033549cb277637d139a9684146a0
sad1qu <adrian635majchrowski@gmail.com>
2026-03-30 21:39:10 +0200

    fix port for render

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello DevOps 🚀');
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('App running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App running on port ${PORT}`);
 });
