74b3642d0c142116ea98490a92278090b5120fc4
sad1qu <adrian635majchrowski@gmail.com>
2026-03-30 21:59:38 +0200

    fix route placement

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`App running on port ${PORT}`);
-
-console.log("SERVER STARTED 🔥");
 app.get('/', (req, res) => {
   console.log('ROOT HIT 🔥');
   res.send('Hello DevOps 🚀');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App running on port ${PORT}`);
+  console.log("SERVER STARTED 🔥");
 });
